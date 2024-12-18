9d4dccb75f5de62f5d35832b735be0658ae73f18
Kavinesh_1 <kavinesh.p123@gmail.com>
2024-12-18 15:31:42 +0530

    Edited the package.json file

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import SplashScreen from "./components/SplashScreen";
 import LoginSignup from "./components/LoginSignup";
 import "./App.css";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<SplashScreen />} />
-        <Route path="/login" element={<LoginSignup />} />
-      </Routes>
-    </Router>
+    <Routes>
+      <Route path="/" element={<SplashScreen />} />
+      <Route path="/login" element={<LoginSignup />} />
+    </Routes>
   );
 }
 
